4f5b1a6fc60178b09245c9f06ebcfa17a737ff7c
Egor Matsegora <yegor.matsegora@gmail.com>
2022-01-07 23:31:14 +0300

    fix typo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publickPath: process.env.NODE_ENV === 'production' ? '/vue-g-map/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-g-map/' : '/',
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
